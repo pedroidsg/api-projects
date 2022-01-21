1b97d6efbd7178470598517d2d314102a368a23e
pedroidsg <pedroidsg@al.infnet.edu.br>
2022-01-20 18:50:13 -0600

    Update movies.py

---
+++ Movies_API/movies.py
@@ -0,0 +1,12 @@
+movies = [
+    {"movie_01": "title_01", "rate_01": 5, "rank_01": 6},
+    {"movie_02": "title_02", "rate_02": 10, "rank_02": 1},
+    {"movie_03": "title_03", "rate_03": 7, "rank_03": 4},
+    {"movie_04": "title_04", "rate_04": 6, "rank_04": 5},
+    {"movie_05": "title_05", "rate_05": 3, "rank_05": 8},
+    {"movie_06": "title_06", "rate_06": 2, "rank_06": 9},
+    {"movie_07": "title_07", "rate_07": 1, "rank_07": 10},
+    {"movie_08": "title_08", "rate_08": 4, "rank_08": 7},
+    {"movie_09": "title_09", "rate_09": 8, "rank_09": 3},
+    {"movie_10": "title_10", "rate_10": 9, "rank_10": 2}
+    ]
